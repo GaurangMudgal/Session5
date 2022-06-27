28e1afc894490a749558ceed2e68b025fc10a741
Gaurang Mudgal <gaurangmudgal13@gmail.com>
2022-06-27 13:02:30 +0530

    First commit

--- src/com/gaurang/session5/Main.java
+++ src/com/gaurang/session5/Main.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class Main {
 
